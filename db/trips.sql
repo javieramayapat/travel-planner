dcf7b4e60e2b2cf76782ed4f74a846fdc89a0442
Javier Amaya <Javieramayapat@gmail.com>
2022-07-21 23:24:15 -0500

    [fix] add command to switch to the project databse

--- db/trips.sql
+++ db/trips.sql
@@ -1,5 +1,8 @@
 CREATE DATABASE travel_planner;
 
+-- switch connection to a new database
+\c travel_planner;
+
 CREATE TABLE users(
     id bigserial NOT NULL,
     name varchar(50) NOT NULL,
@@ -26,5 +29,5 @@ CREATE TABLE trips (
 INSERT INTO users (id, name, last_name, username, email, password) VALUES (1, 'Javier', 'Amaya','javieramayapat', 'javier@gmail.com', 'easypassword');
 
 
-INSERT INTO trips(id, user_id, origin_name, destination_name, departure_date, arrival_date) VALUES (1, 1,'Mexico','Spain','2021-07-21 05:25:01','2021-07-21 08:25:01');
-INSERT INTO trips(id, user_id, origin_name, destination_name, departure_date, arrival_date) VALUES (2, 1,'Spain','France','2021-07-22 10:25:01','2021-07-21 08:25:01');
+-- INSERT INTO trips(id, user_id, origin_name, destination_name, departure_date, arrival_date) VALUES (1, 1,'Mexico','Spain','2021-07-21 05:25:01','2021-07-21 08:25:01');
+-- INSERT INTO trips(id, user_id, origin_name, destination_name, departure_date, arrival_date) VALUES (2, 1,'Spain','France','2021-07-22 10:25:01','2021-07-21 08:25:01');
