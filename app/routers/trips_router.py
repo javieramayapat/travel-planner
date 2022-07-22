ac6390b6f32d8c76d274c3d8cced9399dd86708e
Javier Amaya <Javieramayapat@gmail.com>
2022-07-21 21:40:10 -0500

    [feat] Add path operations to get all the trips and trip detail

--- app/routers/trips_router.py
+++ app/routers/trips_router.py
@@ -1,16 +1,27 @@
-from fastapi import APIRouter, status, Path
+from fastapi import APIRouter, HTTPException, status, Path, Depends
+from sqlalchemy.orm import Session
 
-router = APIRouter(prefix="/trips", tags=['Trips'])
+from ..dependencies import get_db
+from ..schemas import trip_schema
+from ..services import trip_service
+
+router = APIRouter(prefix="/trips",
+                   tags=['Trips'],
+                   dependencies=[Depends(get_db)])
 
 
-@router.get(path="/", status_code=status.HTTP_200_OK)
-def get_trips():
-    pass
+@router.get(path="/", response_model=list[trip_schema.TripOut], status_code=status.HTTP_200_OK)
+def get_trips(db: Session = Depends(get_db)):
+    trips = trip_service.get_trips(db=db)
+    return trips
 
 
-@router.get(path="/{trip_id}", status_code=status.HTTP_200_OK)
-def get_trip_detail(trip_id: int = Path(..., gt=0, example=1)):
-    return {"trip_id": trip_id}
+@router.get(path="/{trip_id}", response_model=trip_schema.TripOut, status_code=status.HTTP_200_OK)
+def get_trip_detail(db: Session = Depends(get_db), trip_id: int = Path(..., gt=0, example=1)):
+    trip = trip_service.get_trip(db=db, trip_id=trip_id)
+    if trip is None:
+        raise HTTPException(status_code=404, detail="Trip Not Found")
+    return trip
 
 
 @router.post(path="/", status_code=status.HTTP_201_CREATED)
