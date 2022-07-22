9403a80004fa6a379ceb05c28e7e10777c97b2ef
Javier Amaya <Javieramayapat@gmail.com>
2022-07-21 20:44:04 -0500

    [feat] Add trip services

---
+++ app/services/trip_service.py
@@ -0,0 +1,15 @@
+from sqlalchemy.orm import Session
+
+from ..models import Trip
+
+
+def get_trips(db: Session):
+    return db.query(Trip).all()
+
+
+def get_trip(db: Session, trip_id: int):
+    return db.query(Trip).filter(Trip.id == trip_id).first()
+
+
+def create_trip(db: Session):
+    pass
